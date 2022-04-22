8a30f8d43c0bf576e982bac19b0bac608c4d9a96
mazharul_13@yahoo.com <Mazharul>
2022-04-22 16:07:27 +0600

    334

--- abc.py
+++ abc.py
@@ -3,3 +3,9 @@ print("This will igonre from git push") 1
 print("This will igonre from git push") 2
 
 print("This will igonre from git push") 3
+
+print("This will igonre from git push") 1
+
+print("This will igonre from git push") 2
+
+print("This will igonre from git push") 3
